66e1119710bea96c90bc891c94b8af444889501f
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-04-15 19:39:52 +0000

    added filter to only arrow keys, added conditionals for turning, added css classes for transform

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ var $car = document.querySelector('.car');
 
 function handleArrowKeys(event) {
   console.log(event.code);
+  if (event.code.slice(0, 5) !== 'Arrow') {
+    return;
+  }
+
+  var direction = event.code;
+  if (direction === 'ArrowUp') {
+    $car.className = 'car turn-up';
+  } else if (direction === 'ArrowRight') {
+    $car.className = 'car turn-right';
+  } else if (direction === 'ArrowDown') {
+    $car.className = 'car turn-down';
+  } else if (direction === 'ArrowLeft') {
+    $car.className = 'car turn-left';
+  }
 }
 
 document.addEventListener('keydown', handleArrowKeys);
