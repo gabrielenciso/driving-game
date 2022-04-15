3eed1ba58c85dd459861b5bceea77ee9a61d7bb5
gabrielenciso <itsgabriel.enciso@gmail.com>
2022-04-15 22:08:51 +0000

    added spacebar event handler

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 var $car = document.querySelector('.car');
+var move = false;
+var intervalId = null;
 
-function handleArrowKeys(event) {
+function handleKeys(event) {
+  console.log(event.code);
+  if (event.code.slice(0, 5) === 'Arrow') {
+    handleDirection(event.code);
+  }
+
+  if (event.code === 'Space') {
+    moveCar(event.code);
+  }
+}
+
+function handleDirection(eventcode) {
   if (event.code.slice(0, 5) !== 'Arrow') {
     return;
   }
@@ -17,4 +30,15 @@ function handleArrowKeys(event) {
   }
 }
 
-document.addEventListener('keydown', handleArrowKeys);
+function moveCar(eventcode) {
+  if (move === false) {
+    move = true;
+    console.log('moving');
+  } else if (move === true) {
+    move = false;
+    console.log('stopped');
+  }
+}
+
+document.addEventListener('keydown', handleKeys);
+// intervalId = setInterval(moveCar, 16);
